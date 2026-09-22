83a2af6d6f1d0579b58434f5b04806d9716d148e
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2026-09-22 14:35:10 +0700

    Update quartz.ts

--- quartz.ts
+++ quartz.ts
@@ -3,28 +3,7 @@ import {
   loadQuartzLayout,
 } from "./quartz/plugins/loader/config-loader"
 
-import { PageTypes } from "./quartz/plugins"
-import SiteLastUpdated from "./quartz/components/SiteLastUpdated"
-
 const config = await loadQuartzConfig()
-const siteLayout = await loadQuartzLayout()
-
-siteLayout.defaults.beforeBody = [
-  ...(siteLayout.defaults.beforeBody ?? []),
-  SiteLastUpdated(),
-]
-
-// Replace the dispatcher created with the original layout
-config.plugins.emitters = config.plugins.emitters.filter(
-  (plugin) => plugin.name !== "PageTypeDispatcher",
-)
-
-config.plugins.emitters.push(
-  PageTypes.PageTypeDispatcher({
-    defaults: siteLayout.defaults,
-    byPageType: siteLayout.byPageType,
-  }),
-)
 
 export default config
-export const layout = siteLayout
+export const layout = await loadQuartzLayout()
