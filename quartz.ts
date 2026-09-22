3840fc382458259d7011c975866102cb53f73620
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2026-09-22 14:00:22 +0700

    Update quartz.ts

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,13 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import SiteLastUpdated from "./quartz/components/SiteLastUpdated"
 
 const config = await loadQuartzConfig()
 export default config
-export const layout = await loadQuartzLayout()
+const siteLayout = await loadQuartzLayout()
+
+siteLayout.defaults.beforeBody = [
+  ...(siteLayout.defaults.beforeBody ?? []),
+  SiteLastUpdated(),
+]
+
+export const layout = siteLayout
